9872b82369387916f7a5d314105ba5b050769e07
yupoy <yudisthirapoyoh@gmail.com>
2019-09-28 02:27:31 +0700

    fix spawntime below 12

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ function parseTime(s) {
 }
 
 function timetoSpawn() {
-  return parseTime(startTime) - parseTime(currentTime);
+  if (parseTime(currentTime) < 720) {
+    return parseTime(startTime) - 720 - parseTime(currentTime);
+  } else {
+    return parseTime(startTime) - parseTime(currentTime);
+  }
 }
 
 function notifSpawn(msg) {
